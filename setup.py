9fa98e87d1b3f40097d110c93bef40f608e7288a
ChlorideCull <steamruler@gmail.com>
2013-08-20 02:06:26 +0000

    Better make sure Mezgr don't get a zillion of my errors now...

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup, find_packages
 setup(
 	name = "TweetPony",
 	version = "1.2.8-py3fork",
-	description = "A Twitter library for Python",
+	description = "A Twitter library for Python 3, ported and maintained by Chloride Cull.",
 	license = "AGPLv3",
-	author = "Julian Metzler",
-	author_email = "contact@mezgrman.de",
-	url = "https://github.com/Mezgrman/TweetPony",
-	keywords = "twitter library api wrapper pony",
+	author = "Julian Metzler, Chloride Cull",
+	author_email = "chloride@devurandom.net",
+	url = "https://github.com/ChlorideCull/TweetPony-Python3",
+	keywords = "twitter library api wrapper pony port",
 	requires = ["requests (<=1.2.0)"],
 	packages = find_packages()
 )
